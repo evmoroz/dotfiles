b63d54612b36a562a033afb554f2447058271ce7
Evgeny Morozov <mail.tyz@gmail.com>
2015-09-09 11:08:01 +0300

    Install script now creates all the required directories

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 # dotfiles install script
 
 # copy the Vim configs and install the plugins
-cp -Rv .vimrc .vim ${HOME}
+cp -Rv .vimrc ${HOME}
+mkdir -vp ${HOME}/.vim/{bundle,tmp}
 git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
