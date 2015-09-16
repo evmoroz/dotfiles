1fa4fd061a297a3885d5388e046a96dd7385f2ff
Evgeny Morozov <mail.tyz@gmail.com>
2015-09-16 21:43:21 +0300

    Changes to the install script

--- install.sh
+++ install.sh
@@ -2,11 +2,13 @@
 # dotfiles install script
 
 # copy the Vim configs and install the plugins
-cp -Rv .vimrc ${HOME}
+cp -v .vimrc ${HOME}
 mkdir -vp ${HOME}/.vim/{bundle,tmp}
-git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
+if [ ! -d ${HOME}/.vim/bundle/Vundle.vim ] ; then
+	git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
+fi
 vim +PluginInstall +qall
 
 # Copy and source the bashrc
-cp .bashrc ${HOME}
+cp -v .bashrc ${HOME}
 source ${HOME}/.bashrc
