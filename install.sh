18d48deca058d597a56221752f8d7978f46eacef
Evgeny Morozov <mail.tyz@gmail.com>
2016-03-09 22:34:50 +0300

    Don't hide files

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # dotfiles install script
 
 # Link the Vim configs and install the plugins
-ln -sfv $(pwd)/.vimrc ${HOME}/
+ln -sfv $(pwd)/vimrc ${HOME}/.vimrc
 mkdir -vp ${HOME}/.vim/{bundle,tmp}
 if [ ! -d ${HOME}/.vim/bundle/Vundle.vim ] ; then
 	git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
@@ -10,12 +10,12 @@ fi
 vim +PluginInstall +qall
 
 # Link the bashrc
-ln -sfv $(pwd)/.bashrc ${HOME}/
+ln -sfv $(pwd)/bashrc ${HOME}/.bashrc
 
 # Link the tmux config
-ln -sfv $(pwd)/.tmux.conf ${HOME}/
+ln -sfv $(pwd)/tmux.conf ${HOME}/.tmux.conf
 
 # link mintty if on cygwin
 if [[ $(uname -s) =~ ^CYGWIN* ]] ; then
-	ln -sfv $(pwd)/.minttyrc ${HOME}/
+	ln -sfv $(pwd)/minttyrc ${HOME}/.minttyrc
 fi
