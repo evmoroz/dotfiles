c16dee020ec5b2fd127af02ea475684af9fa4bb0
Evgeny Morozov <mail.tyz@gmail.com>
2016-03-09 22:42:11 +0300

    Clean before installing new plugins

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkdir -vp ${HOME}/.vim/{bundle,tmp}
 if [ ! -d ${HOME}/.vim/bundle/Vundle.vim ] ; then
 	git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
 fi
-vim +PluginInstall +qall
+vim +PluginClean +PluginInstall +qall
 
 # Link the bashrc
 ln -sfv $(pwd)/bashrc ${HOME}/.bashrc
