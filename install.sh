6eb7d8a6f53f7953802dcce96c9e06d2e70c33d7
Evgeny Morozov <mail.tyz@gmail.com>
2016-03-09 12:41:41 +0300

    mintty config

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #/bin/bash +x
 # dotfiles install script
 
-# copy the Vim configs and install the plugins
-cp -v .vimrc ${HOME}
+# Link the Vim configs and install the plugins
+ln -sfv $(pwd)/.vimrc ${HOME}/
 mkdir -vp ${HOME}/.vim/{bundle,tmp}
 if [ ! -d ${HOME}/.vim/bundle/Vundle.vim ] ; then
 	git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
 fi
 vim +PluginInstall +qall
 
-# Copy and source the bashrc
-cp -v .bashrc ${HOME}
-source ${HOME}/.bashrc
+# Link the bashrc
+ln -sfv $(pwd)/.bashrc ${HOME}/
 
-# Copy tmux config
-cp -v .tmux.conf ${HOME}
+# Link the tmux config
+ln -sfv $(pwd)/.tmux.conf ${HOME}/
+
+# link mintty if on cygwin
+if [[ $(uname -s) =~ ^CYGWIN* ]] ; then
+	ln -sfv $(pwd)/.minttyrc ${HOME}/
+fi
