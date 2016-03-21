cf284b1ff05d594f63e57ff06dda13eb3f8d807a
Evgeny Morozov <mail.tyz@gmail.com>
2016-03-21 17:39:54 +0300

    Configuration for wget

--- install.sh
+++ install.sh
@@ -18,6 +18,9 @@ ln -sfv $(pwd)/bashrc ${HOME}/.bashrc
 # Link the tmux config
 ln -sfv $(pwd)/tmux.conf ${HOME}/.tmux.conf
 
+# Link the wget config
+ln -sfv $(pwd)/wgetrc ${HOME}/.wgetrc
+
 # link mintty if on cygwin
 if [[ $(uname -s) =~ ^CYGWIN* ]] ; then
 	ln -sfv $(pwd)/minttyrc ${HOME}/.minttyrc
