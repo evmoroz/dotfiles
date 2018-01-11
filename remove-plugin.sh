0d628881633bf0d70d42b33ceff812786f59d76c
Evgeny Morozov <Evgeny.Morozov@t-systems.com>
2018-01-11 09:29:02 +0300

    script to remove plugins

---
+++ remove-plugin.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+plugin=${1}
+plugindir="$(dirname $0)/vim/bundle"
+
+if [ -z ${plugin} ]; then
+	echo "Select plugin (${plugindir})"
+	for pluginname in $(ls ${plugindir}); do
+		echo -e "${pluginname} \e[90m[$(git --git-dir=${plugindir}/${pluginname}/.git remote get-url origin)]\e[0m"
+	done
+fi
+
+pluginpath="${plugindir}/${plugin}"
+
+echo "Removing ${pluginpath}..."
+
+if [ ! -d "${pluginpath}" ]; then
+	echo "Could not find ${plugin}."
+	exit 1
+fi
+
+git submodule deinit ${pluginpath}
+git rm ${pluginpath}
+rm -Rf .git/modules/${pluginpath}
+git commit -m "Remove plugin: ${plugin}"
