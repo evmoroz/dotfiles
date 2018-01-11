79b1559551f298bb53785a1fde69efc3f90f458b
Evgeny Morozov <Evgeny.Morozov@t-systems.com>
2018-01-11 10:29:56 +0300

    Allow for optional plugins

--- add-plugin.sh
+++ add-plugin.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-plugindir="vim/pack/ttyz/start"
+execution=start
+eval set -- $(getopt -o "o" --long "opt" -n $0 -- $@)
+while true; do
+	case "${1}" in
+		-o | --opt) execution=opt; shift;;
+		--) shift; break ;;
+		*) echo Unknown option: ${1}; exit 1
+   	esac
+done
+
+
+plugindir="vim/pack/ttyz/${execution}"
 pluginfulldir="$(dirname $0)/${plugindir}"
 cd $(dirname $0)
 pluginurl=${1}
