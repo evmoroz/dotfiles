751a5d9fdb3739fdf685fa8b2b3a8f5aed03983b
Evgeny Morozov <Evgeny.Morozov@t-systems.com>
2018-01-11 11:10:42 +0300

    Update remove plugins script

--- remove-plugin.sh
+++ remove-plugin.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 plugin=${1}
-plugindir="$(dirname $0)/vim/bundle"
+plugindir="$(dirname $0)/vim/pack/ttyz"
 
 if [ -z ${plugin} ]; then
-	echo "Select plugin (${plugindir})"
-	for pluginname in $(ls ${plugindir}); do
-		echo -e "${pluginname} \e[90m[$(git --git-dir=${plugindir}/${pluginname}/.git remote get-url origin)]\e[0m"
+	for pluginname in $(ls ${plugindir}/start); do
+		echo -e "${pluginname} \e[90m[$(git --git-dir=${plugindir}/start/${pluginname}/.git remote get-url origin)]\e[0m"
+	done
+
+	echo -e "\nOptional plugins:"
+	for pluginname in $(ls ${plugindir}/opt); do
+		echo -e "${pluginname} \e[90m[$(git --git-dir=${plugindir}/opt/${pluginname}/.git remote get-url origin)]\e[0m"
 	done
 	exit 0
 fi
 
-pluginpath="${plugindir}/${plugin}"
+pluginstartpath="${plugindir}/start/${plugin}"
+pluginoptpath="${plugindir}/opt/${plugin}"
 
 echo "Removing ${pluginpath}..."
 
-if [ ! -d "${pluginpath}" ]; then
+if [ -d "${pluginstartpath}" ]; then
+	pluginpath=${pluginstartpath}
+	
+fi
+
+if [ -d "${pluginoptpath}" ]; then
+	pluginpath=${pluginoptpath}
+fi
+
+if [ -z "${pluginpath}" ]; then
 	echo "Could not find ${plugin}."
 	exit 1
 fi
