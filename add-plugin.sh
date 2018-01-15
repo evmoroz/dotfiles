370fde9e407d82121ad5e443fb5be2e0b7f9b3fe
Evgeny Morozov <Evgeny.Morozov@t-systems.com>
2018-01-15 12:37:29 +0300

    Support local plugins

--- add-plugin.sh
+++ add-plugin.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 execution=start
-while getopts o name; do
+path=ttyz
+while getopts ol name; do
 	case "${1}" in
 		o) execution=opt;;
+		o) path=local;;
 		*) echo Unknown option: ${1}; exit 1
    	esac
 done
 
 shift $(($OPTIND -1))
 
-plugindir="vim/pack/ttyz/${execution}"
+plugindir="vim/pack/${path}/${execution}"
 pluginfulldir="$(dirname $0)/${plugindir}"
 cd $(dirname $0)
 pluginurl=${1}
@@ -26,9 +28,13 @@ pluginpath="${pluginfulldir}/${pluginname}"
 
 echo Installing ${plugin}
 
-git submodule init
-git submodule add ${pluginurl} ${pluginpath}
-git add .gitmodules ${plugindir}/${pluginname}
-git commit -m "Install plugin: ${pluginname}"
+if [ "${path}" != "local" ]; then
+	git submodule init
+	git submodule add ${pluginurl} ${pluginpath}
+	git add .gitmodules ${plugindir}/${pluginname}
+	git commit -m "Install plugin: ${pluginname}"
+else
+	git clone ${pluginurl} ${pluginpath}
+fi
 
 vim +":helptags ${pluginpath}" +":q"
