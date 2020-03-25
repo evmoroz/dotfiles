11b7144acdeeb5a77eff6f3a7fe0f1016a042d82
Evgeny Morozov <mail.tyz@gmail.com>
2020-03-25 08:07:54 +0100

    update for kitty 0.17

--- config/kitty/status.py
+++ config/kitty/status.py
@@ -1,5 +1,6 @@
 from types import MethodType
 from kitty.tab_bar import as_rgb
+from kitty.rgb import color_as_int, to_color
 
 def main(args):
     pass
@@ -10,16 +11,17 @@ def update_wrapper(bar):
         original(*data)
         screen = self.screen
         status_len = sum([ s.find('|') + 3 for s in self.status_blocks])
+        self.screen.cursor.bg = as_rgb(color_as_int(self.draw_data.default_bg))
         self.screen.draw(' '*(screen.columns - screen.cursor.x - status_len))
         for block in self.status_blocks:
             fields = block.split('|')
             text = fields[0]
-            delimiter = chr(int(fields[3], 16))
-            bg = int(fields[1], 16)
+            delimiter = fields[3][0]
+            bg = color_as_int(to_color(fields[1]))
             self.screen.cursor.fg = screen.cursor.bg
             self.screen.cursor.bg = as_rgb(bg)
             self.screen.draw(delimiter)
-            fg = int(fields[2], 16)
+            fg = color_as_int(to_color(fields[2]))
             self.screen.cursor.fg = as_rgb(fg)
             self.screen.draw(" " + text + " ")
         # self.screen.draw(u'\ue0d4')
